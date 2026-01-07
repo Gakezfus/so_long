44fef5f80df779ac9694e13bb1e44d23711150f1
Elkan <echoo@42mail.sutd.edu.sg>
2026-01-08 01:36:50 +0800

    trying to import images is causing a segfault, TBD

--- open_window.c
+++ open_window.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   open_window.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: Elkan Choo <echoo@42mail.sutd.edu.sg>      +#+  +:+       +#+        */
+/*   By: elkan <elkan@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/07 12:25:03 by elkan             #+#    #+#             */
-/*   Updated: 2026/01/07 18:37:28 by Elkan Choo       ###   ########.fr       */
+/*   Updated: 2026/01/08 01:36:39 by elkan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,20 +19,29 @@
 
 //-I.mlx -L./.mlx -lmlx -lXext -lX11
 
-typedef struct parameters
+typedef struct img
+{
+	void	*name;
+	int		x;
+	int		y;
+}	t_img;
+
+typedef struct pareters
 {
 	void	*mlx;
-	void	*window;
+	void	*wind;
 	char	**map;
 	int		width;
 	int		height;
+	int		size;
+	t_img	*player;
 }	t_pars;
 
 int		open_window(char **map, int width, int height);
 int		handle_keys(int keycode, void *p_ptr);
 int		handle_mouse(int button, int x, int y, void *p_ptr);
 int		close_window(void *p_ptr);
-void	setup(t_pars *param, int width, int height);
+void	setup(t_pars *par, int width, int height);
 
 // int	main(void)
 // {
@@ -45,53 +54,56 @@ int	open_window(char **map, int width, int height)
 {
 	void	*mlx;
 	void	*window;
-	t_pars	*param;
+	t_pars	*par;
 
 	mlx = mlx_init();
-	param = malloc(sizeof(t_pars));
-	if (mlx == NULL || param == NULL)
+	par = malloc(sizeof(t_pars));
+	if (mlx == NULL || par == NULL)
 		return (free(mlx), 1);
-	param->mlx = mlx;
-	param->map = map;
-	window = mlx_new_window(mlx, width * 50,
-			height * 50, "Window");
+	par->mlx = mlx;
+	par->map = map;
+	par->size = 50;
+	window = mlx_new_window(mlx, width * par->size,
+			height * par->size, "Window");
 	if (window == NULL)
 		return (1);
-	param->window = window;
-	setup(param, width, height);
-	mlx_key_hook(window, handle_keys, (void *)param);
-	mlx_mouse_hook(window, handle_mouse, (void *)param);
-	mlx_hook(window, 17, 0, close_window, (void *)param);
+	par->wind = window;
+	setup(par, width, height);
+	mlx_key_hook(window, handle_keys, (void *)par);
+	mlx_mouse_hook(window, handle_mouse, (void *)par);
+	mlx_hook(window, 17, 0, close_window, (void *)par);
 	mlx_loop(mlx);
 	return (0);
 }
 
-void	setup(t_pars *param, int width, int height)
+void	setup(t_pars *par, int width, int height)
 {
-	int		index;
-	int		index_2;
+	int		y;
+	int		x;
 
-	param->width = width;
-	param->height = height;
-	index = 0;
-	while (index < param->height * 50)
+	par->width = width;
+	par->height = height;
+	y = 0;
+	// Causes segfault
+	// par->player->name = mlx_xpm_file_to_image(par->mlx, "./images/Roomba.xpm", &par->player->x, &par->player->y);
+	while (y < par->height * par->size)
 	{
-		index_2 = 0;
-		while (index_2 < param->width * 50)
+		x = 0;
+		while (x < par->width * par->size)
 		{
-			if (param->map[index / 50][index_2 / 50] == '0')
-				mlx_pixel_put(param->mlx, param->window, index_2, index, 0x000077AA);
-			else if (param->map[index / 50][index_2 / 50] == '1')
-				mlx_pixel_put(param->mlx, param->window, index_2, index, 0x00555555);
-			else if (param->map[index / 50][index_2 / 50] == 'P')
-				mlx_pixel_put(param->mlx, param->window, index_2, index, 0x00999900);
-			else if (param->map[index / 50][index_2 / 50] == 'C')
-				mlx_pixel_put(param->mlx, param->window, index_2, index, 0x00005500);
-			else if (param->map[index / 50][index_2 / 50] == 'E')
-				mlx_pixel_put(param->mlx, param->window, index_2, index, 0x00550055);
-			index_2++;
+			if (par->map[y / par->size][x / par->size] == '0')
+				mlx_pixel_put(par->mlx, par->wind, x, y, 0x000077AA);
+			else if (par->map[y / par->size][x / par->size] == '1')
+				mlx_pixel_put(par->mlx, par->wind, x, y, 0x00555555);
+			// else if (par->map[y / par->size][x / par->size] == 'P')
+			// 	mlx_put_image_to_window(par->mlx, par->wind, player, x, y);
+			else if (par->map[y / par->size][x / par->size] == 'C')
+				mlx_pixel_put(par->mlx, par->wind, x, y, 0x00005500);
+			else if (par->map[y / par->size][x / par->size] == 'E')
+				mlx_pixel_put(par->mlx, par->wind, x, y, 0x00550055);
+			x++;
 		}
-		index++;
+		y++;
 	}
 }
 
@@ -105,21 +117,21 @@ int	handle_mouse(int button, int x, int y, void *p_ptr)
 
 int	close_window(void *p_ptr)
 {
-	t_pars	*param;
+	t_pars	*par;
 
-	param = (t_pars *)p_ptr;
-	mlx_destroy_window(param->mlx, param->window);
+	par = (t_pars *)p_ptr;
+	mlx_destroy_window(par->mlx, par->wind);
 	exit(0);
 }
 
 int	handle_keys(int keycode, void *p_ptr)
 {
-	t_pars	*param;
+	t_pars	*par;
 
-	param = (t_pars *)p_ptr;
+	par = (t_pars *)p_ptr;
 	if (keycode == 0xff1b)
 	{
-		mlx_destroy_window(param->mlx, param->window);
+		mlx_destroy_window(par->mlx, par->wind);
 		exit(0);
 	}
 	return (0);
